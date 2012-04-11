3d3798aa1136e9867156083a194aa9c21e304fa1
Zack Yang <zack9433@gmail.com>
2012-04-11 20:47:07 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ require.config({
     order: '../assets/js/require/plugin/order',
     text: '../assets/js/require/plugin/text',
     tpl: '../assets/js/require/plugin/tpl',
+    domReady: '../assets/js/require/plugin/domReady',
     templates: 'templates'
   }
 });
@@ -21,13 +22,19 @@ require([
 
   // Load our app module and pass it to our definition function
   'app',
+  'domReady',
   '../assets/js/jquery/jquery-1.7.2.min',
   // Some plugins have to be loaded in order due to there non AMD compliance
   // Because these scripts are not "modules" they do not pass any values to the definition function below
   'order!../assets/js/underscore/underscore-min',
   'order!../assets/js/backbone/backbone-min'
 
-], function(App){
-  // The "app" dependency is passed in as "App"
-  App.initialize();
+], function(App, domReady){
+  //This function is called once the DOM is ready.
+  //It will be safe to query the DOM and manipulate
+   //DOM nodes in this function.
+  domReady(function () {
+    // The "app" dependency is passed in as "App"
+    App.initialize();
+  });
 });
